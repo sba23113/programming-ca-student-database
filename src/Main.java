f279288d72f1fb37cc96ef2d62f72251ed425c47
LHomola <lhomola83@gmail.com>
2024-02-01 14:15:02 +0000

    create main menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String inFilename = "students.txt";
+        String outFilename = "status.txt";
+        int choice;
+
+        while (true) {
+            System.out.println();
+            System.out.println("**********************************************************************");
+            System.out.println("STUDENT DATABASE");
+            System.out.println("**********************************************************************");
+            System.out.println();
+            System.out.println("Available actions:");
+            System.out.println("1: Update \"status.txt\" file using data from \"students.txt\" file");
+            System.out.println("2: Enter student details manually");
+            System.out.println("0: Exit");
+            System.out.println();
+            System.out.print("What would you like to do: ");
+            choice = scanner.nextInt();
+            System.out.println();
+
+            if (choice == 0) {
+                break;
+            } else if (choice == 1) {
+                System.out.println("choice 1");
+            } else if (choice == 2) {
+                System.out.println("choice 2");
+            }
+        }
     }
 }
