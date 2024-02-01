5fec520f7ae471da495a3eeed03b02bba68d52d9
LHomola <lhomola83@gmail.com>
2024-02-01 23:38:42 +0000

    add while loop to readFromFile function
    iterating through input file

--- src/Main.java
+++ src/Main.java
@@ -2,102 +2,165 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    // does the string contain a name with this regex pattern: word (letters only), space, word (alphanumeric characters)
+    // first line -> regex pattern: word (letters only), space, word (alphanumeric characters)
     public static Boolean isNameValid(String nameString) {
         if (nameString.matches("^[a-zA-Z]+[\\s][\\w]+$")) {
-            System.out.println("First name and last name OK");
             return true;
         } else {
-            System.out.println("First name and last name NOT OK");
+            System.out.println("\nFollowing entry will not be written to output file:");
+            System.out.printf("Student name: %s", nameString);
+            System.out.println("\nReason: Student name format invalid");
+            System.out.println("Correct format: Word (letters only), space, word (alphanumeric characters)");
             return false;
         }
     }
 
-    // does next line contain number between 1 and 8 (incl.)
-    public static Boolean isClassCountValid(String classString) {
+    // second line -> confirm next line contains number between 1 and 8 (incl.)
+    public static Boolean isClassCountValid(String classString, String studentName) {
         try {
             int classCount = Integer.parseInt(classString);
             if (classCount < 1 || classCount > 8) {
-                System.out.println("Number of classes must be between 1 and 8 inclusive");
+                System.out.println("\nFollowing entry will not be written to output file:");
+                System.out.printf("Student name: %s", studentName);
+                System.out.println("\nReason: Number of classes must be between 1 and 8 inclusive");
                 return false;
             } else {
-                System.out.println("Class info OK");
                 return true;
             }
         } catch (Exception e) {
-            System.out.println("Second line is not a number");
+            System.out.println("\nFollowing entry will not be written to output file:");
+            System.out.printf("Student name: %s", studentName);
+            System.out.println("\nReason: Second line is not a number\n");
             return false;
         }
     }
 
-    public static Boolean isStudentNumberValid(String studentCode) {
-        int year = Integer.parseInt(studentCode.substring(0, 2));
+    // third line -> regex pattern: (number) 2, (number) 0-4, letter, letter, letter(?), (number) 1-200
+    public static Boolean isStudentNumberValid(String studentCode, String studentName) {
+        int year;
+        try {
+            year = Integer.parseInt(studentCode.substring(0, 2));
+        } catch (Exception e) {
+            System.out.println("\nFollowing entry will not be written to output file:");
+            System.out.printf("Student name: %s", studentName);
+            System.out.printf("\nReason: Student number format invalid (%s)", studentCode);
+            System.out.println("\nCorrect format: Last two digits of a year (2020-2024) + course type (MSC, DIP, etc.) + ID number between 1 and 200 (incl.)");
+            return false;
+        }
 
         String courseCode;
         int studentID;
-        if (Character.isLetter(studentCode.charAt(4))) {
-            courseCode = studentCode.substring(2, 5);
-            studentID = Integer.parseInt(studentCode.substring(5));
+        String studentIdStr;
+        if (studentCode.length() > 4) {
+            if (Character.isLetter(studentCode.charAt(4))) {
+                courseCode = studentCode.substring(2, 5);
+                studentIdStr = studentCode.substring(5);
+            } else {
+                courseCode = studentCode.substring(2, 4);
+                studentIdStr = studentCode.substring(4);
+            }
         } else {
-            courseCode = studentCode.substring(2, 4);
-            studentID = Integer.parseInt(studentCode.substring(4));
+            System.out.println("\nFollowing entry will not be written to output file:");
+            System.out.printf("Student name: %s", studentName);
+            System.out.printf("\nReason: Student number format invalid (%s)", studentCode);
+            System.out.println("\nCorrect format: Last two digits of a year (2020-2024) + course type (MSC, DIP, etc.) + ID number between 1 and 200 (incl.)");
+            return false;
+        }
+
+        // check if courseCode string only contains letters
+        char[] chars = courseCode.toCharArray();
+        for (char c : chars) {
+            if(!Character.isLetter(c)) {
+                System.out.println("\nFollowing entry will not be written to output file:");
+                System.out.printf("Student name: %s", studentName);
+                System.out.printf("\nReason: Student number format invalid (%s)", studentCode);
+                System.out.println("\nCorrect format: Last two digits of a year (2020-2024) + course type (MSC, DIP, etc.) + ID number between 1 and 200 (incl.)");
+                return false;
+            }
+        }
+
+        try {
+            studentID = Integer.parseInt(studentIdStr);
+            if (studentID < 1 || studentID > 200) {
+                System.out.println("\nFollowing entry will not be written to output file:");
+                System.out.printf("Student name: %s", studentName);
+                System.out.printf("\nReason: Student number format invalid (%s)", studentCode);
+                System.out.println("\nCorrect format: Last two digits of a year (2020-2024) + course type (MSC, DIP, etc.) + ID number between 1 and 200 (incl.)");
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println("\nFollowing entry will not be written to output file:");
+            System.out.printf("Student name: %s", studentName);
+            System.out.printf("\nReason: Student number format invalid (%s)", studentCode);
+            System.out.println("\nCorrect format: Last two digits of a year (2020-2024) + course type (MSC, DIP, etc.) + ID number between 1 and 200 (incl.)");
+            return false;
         }
 
         if (year >= 20 && year <= 24) {
-            System.out.printf("student number OK: %d %s %d", year, courseCode, studentID);
-            System.out.println();
-            System.out.println();
             return true;
         } else {
-            System.out.println("student number NOT OK");
+            System.out.println("\nFollowing entry will not be written to output file:");
+            System.out.printf("Student name: %s", studentName);
+            System.out.printf("\nReason: Student number format invalid (%s)", studentCode);
+            System.out.println("\nCorrect format: Last two digits of a year (2020-2024) + course type (MSC, DIP, etc.) + ID number between 1 and 200 (incl.)");
             return false;
         }
     }
 
     public static Boolean readFromFile(String inFilename, String outFilename) {
+        String line;
+        String studentName = "";
+        String yearString = "";
+        String studentNumber;
+        int lineCounter = 0;
+        boolean isInfoValid = false;
         try (BufferedReader inFileReader = new BufferedReader(new FileReader(inFilename))) {
-            String firstLine = inFileReader.readLine();
-
-            // make sure input file is not empty
-            if (firstLine == null) {
-                System.out.println("Error: Input file is empty!");
-                return true;
-            }
-
-            // first line -> regex pattern: word (letters only), space, word (alphanumeric characters)
-            isNameValid(firstLine);
-
-            // second line -> confirm next line contains number between 1 and 8 (incl.)
-            String secondLine = inFileReader.readLine();
-            isClassCountValid(secondLine);
-
-            // third line -> regex pattern: (number) 2, (number) 0-4, letter, letter, letter(?), (number) 1-200
-            String thirdLine = inFileReader.readLine();
-            isStudentNumberValid(thirdLine);
-
-            // write trade details to output file (append mode)
-            try (BufferedWriter outFileReader = new BufferedWriter(new FileWriter(inFilename, true))) {
-                outFileReader.write(thirdLine + " - " + firstLine.split(" ")[1] + "\n");
-                String workloadStr = "";
-                int classCount = Integer.parseInt(secondLine);
-                if (classCount == 1) {
-                    workloadStr = "Very Light";
-                } else if (classCount == 2) {
-                    workloadStr = "Light";
-                } else if (classCount > 2 && classCount < 6) {
-                    workloadStr = "Part Time";
-                } else if (classCount >= 6) {
-                    workloadStr = "Full Time";
+            while ((line = inFileReader.readLine()) != null) {
+                lineCounter += 1;
+                if (lineCounter % 3 == 1) {
+                    // first line -> regex pattern: word (letters only), space, word (alphanumeric characters)
+                    isInfoValid = isNameValid(line);
+                    studentName = line;
+                } else if (lineCounter % 3 == 2 && isInfoValid) {
+                    // second line -> confirm next line contains number between 1 and 8 (incl.)
+                    isInfoValid = isClassCountValid(line, studentName);
+                    yearString = line;
+                } else if (lineCounter % 3 == 0 && isInfoValid) {
+                    // third line -> regex pattern: (number) 2, (number) 0-4, letter, letter, letter(?), (number) 1-200
+                    isInfoValid = isStudentNumberValid(line, studentName);
+                    studentNumber = line;
+                    if (isInfoValid) {
+                        writeToFile(outFilename, studentName, yearString, studentNumber);
+                    }
                 }
-                outFileReader.write(workloadStr + "\n");
-            } catch (IOException e) {
-                System.out.println("Error: Couldn't update " + outFilename + " file!");
             }
         } catch (Exception e) {
+            System.out.println();
             System.out.println("Input file access error!");
         }
 
         return false;
+    }
+
+    public static void writeToFile(String outFilename, String studentName, String yearString, String studentNumber) {
+        // write student details to output file (append mode)
+        try (BufferedWriter outFileReader = new BufferedWriter(new FileWriter(outFilename, true))) {
+            outFileReader.write(studentNumber + " - " + studentName.split(" ")[1] + "\n");
+            String workloadStr = "";
+            int classCount = Integer.parseInt(yearString);
+            if (classCount == 1) {
+                workloadStr = "Very Light";
+            } else if (classCount == 2) {
+                workloadStr = "Light";
+            } else if (classCount > 2 && classCount < 6) {
+                workloadStr = "Part Time";
+            } else if (classCount >= 6) {
+                workloadStr = "Full Time";
+            }
+            outFileReader.write(workloadStr + "\n");
+        } catch (IOException e) {
+            System.out.println("Error: Output file access error!");
+        }
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -123,12 +186,14 @@ public class Main {
             if (choice == 0) {
                 break;
             } else if (choice == 1) {
+                System.out.printf("Updating \"%s\" file... \n", outFilename);
                 if (readFromFile(inFilename, outFilename)) {
                     break;
                 }
             } else if (choice == 2) {
                 System.out.println("choice 2");
             }
+            System.out.println("\nUpdate complete.");
         }
     }
 }
